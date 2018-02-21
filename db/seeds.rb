aac430bfc7082435f9878067bca5ba9a284766a0
francisco gaivao <francisco.abecassis.gaivao@gmail.com>
2018-02-21 16:43:53 +0000

    yelp rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+restaurant = Restaurant.create([{name: 'Triangulo da Ribeira', address:'Lisboa', category:'italian', phone_number: '2148342'}])
+                              ([{name: 'Agrelos', address:'Santos', category:'chinese', phone_number: '22342'}])
+                              ([{name: 'Amigos do Minho', address:'Porto', category:'japanese', phone_number: '2453452'}])
+                              ([{name: 'Catedral', address:'Lisboa', category:'french', phone_number: '25555342'}])
+                              ([{name: 'Sicciliano', address:'Cascais', category:'italian', phone_number: '2144352'}])# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
